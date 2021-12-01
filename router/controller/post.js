e767f62101cc0ad59f153032dcfa5267c6f9849d
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-12-01 19:07:02 +0300

    delete by id

--- router/controller/post.js
+++ router/controller/post.js
@@ -73,14 +73,64 @@ const updateById = async (req, res) => {
           await post.save();
           res.status("200").json(post);
         } else {
-          res.status("404").json("there is no todo with this id");
+          res.status("404").json("there is no post with this id");
         }
       } else {
-        res.status("404").json("can not access this todo");
+        res.status("404").json("can not access this post");
       }
     });
   } catch (err) {
     res.status("404").json(err);
   }
 };
-module.exports = { createPost, getPosts, updateById };
+
+// const updateById = async (req, res) => {
+//   try {
+//     const { desc, id, postId } = req.body;
+//     //find user
+//     await postModel.findOne({ owner: id }).then(async (result) => {
+//       if (result) {
+//         const post = await postModel.findByIdAndUpdate(postId, { desc }); //.then(async (result) => {
+//         if (post) {
+//           console.log(post);
+//           await post.save();
+//           res.status("200").json(post);
+//         } else {
+//           res.status("404").json("there is no todo with this id");
+//         }
+//       } else {
+//         res.status("404").json("can not access this todo");
+//       }
+//     });
+//   } catch (err) {
+//     res.status("404").json(err);
+//   }
+// };
+
+const deleteTodo = async (req, res) => {
+  try {
+    const { id, todoId } = req.params;
+    await userModel.findById(id).then(async (result) => {
+      if (result) {
+        await postModel.find({ owner: id }).then(async (result) => {
+          if (result.length) {
+            await postModel.deleteOne({ id: todoId }).then((result) => {
+              if (result.deletedCount != 0) {
+                res.status("200").json(result);
+              } else {
+                res.status("404").json("already deleted");
+              }
+            });
+          } else {
+            res.status("404").json("this user does not have posts");
+          }
+        });
+      } else {
+        res.status("404").json("no user with this id");
+      }
+    });
+  } catch (err) {
+    res.status("404").json(err);
+  }
+};
+module.exports = { createPost, getPosts, updateById, deleteTodo };
--- router/routes/post.js
+++ router/routes/post.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const routerPost = express.Router();
 
-const { createPost,getPosts,updateById } = require("./../controller/post");
+const { createPost,getPosts,updateById,deleteTodo } = require("./../controller/post");
 // const authentication = require("./../middleware/authentication");
 // const authorization = require("./../middleware/authorization");
 
 routerPost.post("/createPost",  createPost);
 routerPost.get("/getPosts/:id",  getPosts);
 routerPost.post("/updateById",  updateById);
+routerPost.delete("/deleteTodo/:id/:todoId",deleteTodo);
 module.exports = routerPost;
