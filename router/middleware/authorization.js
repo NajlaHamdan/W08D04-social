eb3445304972498c6e354d7809a0255d9b9fa2dd
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-12-19 00:23:42 +0300

    update auth

--- router/middleware/authorization.js
+++ router/middleware/authorization.js
@@ -1,8 +1,9 @@
 const roleModel = require("./../../db/models/role");
 const authorization = async (req, res, next) => {
   try {
-    const roleId = req.token.role;
+    const roleId = req.tocken.role;
     const result = await roleModel.findById(roleId);
+    console.log(result);
     if (result.role === "admin") {
       next();
     } else {
